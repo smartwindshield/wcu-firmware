c34a2dbd5b63570fb46d20af56b2edd020178afd
alejzeis <5985254+alejzeis@users.noreply.github.com>
2024-10-24 15:23:46 -0500

    Constants for I2C Bus and Baud rate

--- firmware/include/sensors/sensors_controller.h
+++ firmware/include/sensors/sensors_controller.h
@@ -1,6 +1,11 @@
 #ifndef SENSORS_CONTROLLER_H
 #define SENSORS_CONTROLLER_H
 
+#include "HAL_I2C.h"
+
+static const HAL_I2C_Bus SENSORS_I2C_BUS = HAL_I2C_BUS_0;
+static const int SENSORS_I2C_BAUD_RATE = 100000;
+
 void SensorsController_Init(void);
 
 /**
--- firmware/src/sensors/sensors_controller.c
+++ firmware/src/sensors/sensors_controller.c
@@ -2,11 +2,8 @@
 
 #include "HAL_I2C.h"
 
-static const HAL_I2C_Bus I2C_BUS = HAL_I2C_BUS_0;
-static const int I2C_BAUD_RATE = 100000;
-
 void SensorsController_Init(void) {
-    HAL_I2C_Init(I2C_BUS, I2C_BAUD_RATE);
+    HAL_I2C_Init(SENSORS_I2C_BUS, SENSORS_I2C_BAUD_RATE);
 }
 
 void SensorsController_Update(void) {
