8c8ce04af785f5a5ae1bc95c5154316f2b6c0934
alejzeis <5985254+alejzeis@users.noreply.github.com>
2024-10-25 12:09:25 -0500

    Bluetooth: Dynamic number of characteristics
    Support registration of multiple characteristics,
    Support for client to read and write them.

--- core-pico/src/main.c
+++ core-pico/src/main.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-
 #include "pico/cyw43_arch.h"
 
 #include "led_controller.h"
@@ -14,13 +12,10 @@ int main() {
     BluetoothComms_Init();
     SensorsController_Init();
 
-    // Flash the network chip LED just to see something
-    static int led_on = true;
+    // Turn on LED after all modules initialized
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    
     for (;;) {
-        led_on = !led_on;
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-        sleep_ms(500);
-
         SensorsController_Update();
         LEDController_Update();
     }
