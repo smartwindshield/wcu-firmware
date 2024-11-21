fc09b1014e1a9739bb7fa72cd9b67aca59900a51
alejzeis <5985254+alejzeis@users.noreply.github.com>
2024-11-20 20:54:21 -0600

    LED: Add framework for updating LEDs every 1 seconds

--- firmware/src/led_controller.c
+++ firmware/src/led_controller.c
@@ -1,9 +1,19 @@
 #include "led_controller.h"
 #include "HAL_GPIO.h"
 #include "HAL_Neopixels.h"
+#include "HAL_Time.h"
 
 // Two strips in series of 167 each
 static const size_t NUM_PIXELS = 167 * 2;
+
+// Update which LEDs are on/off at this interval
+static const uint32_t LED_UPDATE_INTERVAL_MS = 1000;
+
+/**
+ * Set which LEDs are on/off in accordance to the data obtained
+ * from the Solar module.
+ */
+static void SetLEDs(void);
 
 void LEDController_Init(void) {    
     HAL_Neopixels_RGB color = {255, 255, 255};
@@ -15,5 +25,17 @@ void LEDController_Init(void) {
 }
 
 void LEDController_Update(void) {
-    
+     // Persistant variable to keep track of the last time LEDs were updated
+    static uint32_t lastUpdateCall = 0;
+
+    uint32_t currentTime = HAL_Time_GetTimeNow();
+
+    if (lastUpdateCall == 0 || currentTime - lastUpdateCall >= LED_UPDATE_INTERVAL_MS) {
+        SetLEDs();
+        lastUpdateCall = currentTime;
+    }
+}
+
+static void SetLEDs(void) {
+    // TODO: Set which LEDs are on and off here
 }
