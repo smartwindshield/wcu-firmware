814f632c078d488e8fd417d40622b83c4d333b9f
alejzeis <5985254+alejzeis@users.noreply.github.com>
2024-11-22 15:25:00 -0600

    Barometer: Disable, use hardcoded value

---
+++ firmware/include/wcu_firmware_config.h
@@ -0,0 +1,10 @@
+#ifndef WCU_FIRMWARE_CONFIG_H
+#define WCU_FIRMWARE_CONFIG_H
+
+#define WCU_FIRMWARE_REV_MAJOR 1
+#define WCU_FIRMWARE_REV_MINOR 1
+
+// Disable barometer, PCB footprint incorrect
+#define BAROMETER_DISABLED 1
+
+#endif // WCU_FIRMWARE_CONFIG_H
--- firmware/src/sensors/barometer.c
+++ firmware/src/sensors/barometer.c
@@ -1,34 +1,42 @@
 #include "sensors/barometer.h"
 #include "HAL_Debug.h"
-#include "HAL_GPIO.h"
-#include <stdio.h>
 #include "HAL_I2C.h"
+#include "sensors/sensors_controller.h"
+#include "wcu_firmware_config.h"
 
-#define SDA_PIN 0
-#define SCL_PIN 1
-
-#define GPS_I2C_ADDRESS 0x5C
+static const uint8_t BAROMETER_I2C_ADDRESS = 0x5C;
+static const float DEFAULT_PRESSURE = 1020.9; // Pressure reported at MSP on 11/22/24
 
 void BarometerInit(void){
-    HAL_I2C_Init(HAL_I2C_BUS_0, 100000);
+#ifndef BAROMETER_DISABLED
     uint8_t config[2] = {0x20, 0x90};
 
-    HAL_I2C_Write(HAL_I2C_BUS_0, GPS_I2C_ADDRESS, config, 2);
+    HAL_I2C_Write(SENSORS_I2C_BUS, BAROMETER_I2C_ADDRESS, config, 2);
+
+    HAL_Debug_Printf("[Barometer]: Initialized.\n");
+#else
+    HAL_Debug_Printf("[Barometer]: DISABLED, skipping initialization.\n");
+#endif // BAROMETER_DISABLED
 }
 
 //Pressure in hectopascal
-float BarometerGetData(void){
+float BarometerGetData(void) {
+    float pressure;
+    uint8_t data[3];
     uint8_t reg[1] = {0x28 | 0x80};
 
-    HAL_I2C_Write(HAL_I2C_BUS_0, GPS_I2C_ADDRESS, reg, 1);
+#ifndef BAROMETER_DISABLED
+    HAL_I2C_Write(SENSORS_I2C_BUS, BAROMETER_I2C_ADDRESS, reg, 1);
 
-    uint8_t data[3];
+    HAL_I2C_Read(SENSORS_I2C_BUS, BAROMETER_I2C_ADDRESS, data, 3);
 
-    HAL_I2C_Read(HAL_I2C_BUS_0, GPS_I2C_ADDRESS, data, 3);
+    pressure = ((data[2] << 16) | (data[1] << 8) | data[0]) / 4096.0;
 
-    float pressure = ((data[2] << 16) | (data[1] << 8) | data[0]) / 4096.0;
-
-    HAL_Debug_Printf("[Barometer]: Barometer reports %f hectopascal\n", pressure);
+    HAL_Debug_Printf("[Barometer]: Reporting %f hectopascal\n", pressure);
+#else
+    pressure = DEFAULT_PRESSURE;
+    HAL_Debug_Printf("[Barometer]: DISABLED, reporting %f hectopascal\n", pressure);
+#endif // BAROMETER_DISABLED
 
     return pressure;
 }
