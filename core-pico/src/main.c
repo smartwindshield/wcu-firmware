584b1e07dd31be7381cb8c5f738e4faa453032a0
alejzeis <5985254+alejzeis@users.noreply.github.com>
2024-11-23 19:33:17 -0600

    Measure timing of main loop

--- core-pico/src/main.c
+++ core-pico/src/main.c
@@ -1,3 +1,4 @@
+#include "HAL_Time.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "pico/time.h"
@@ -7,11 +8,16 @@
 #include "sensors/gps.h"
 #include "sensors/sensors_controller.h"
 
+#include <hardware/timer.h>
+#include <pico/types.h>
 #include <stdnoreturn.h>
 
 #define STARTUP_DELAY 1000
 
-static const int ERROR_NO_GPS_LED_PERIOD = 200;
+// Make this a power of 2 for quick division
+#define NUM_TIMING_SAMPLES 2
+
+static const int ERROR_NO_GPS_LED_PERIOD = 100;
 
 static noreturn void errorHandler(int period) {
     printf("[errorHandler]: Error Handler invoked\n");
@@ -24,7 +30,41 @@ static noreturn void errorHandler(int period) {
     }
 }
 
+/**
+ * Calculates the average time the main loop takes to run.
+ *
+ * Stores a certain amount of samples, and then averages them.
+ */
+static void measureTiming(uint64_t startTime) {
+    static uint64_t measurements[NUM_TIMING_SAMPLES] = {0, };
+    static int currentIndex = 0;
+    static uint64_t sumTime = 0;
+    uint32_t currentTiming = 0;
+    uint32_t averageTime = 0;
+
+    uint64_t currentTime = time_us_64();
+
+    currentTiming = currentTime - startTime;
+
+    measurements[currentIndex] = currentTiming;
+    sumTime += measurements[currentIndex];
+
+    if (++currentIndex >= NUM_TIMING_SAMPLES) {
+        averageTime = sumTime / NUM_TIMING_SAMPLES;
+
+        printf("[Performance]: Latest timing: %d us, average: %d us.\n", 
+                currentTiming,
+                averageTime);
+
+        currentIndex = 0;
+        sumTime = 0;
+    }
+}
+
 int main() {
+    bool statusLEDState = true;
+    uint64_t startTime;
+
     stdio_init_all();
 
 #if STARTUP_DELAY
@@ -50,8 +90,16 @@ int main() {
     }
     
     for (;;) {
+        startTime = time_us_64();
+
         SensorsController_Update();
         LEDController_Update();
+
+        measureTiming(startTime);
+
+        // Blinking the LED to show activity of the main loop
+        statusLEDState = !statusLEDState;
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, statusLEDState);
     }
 
     return 0;
